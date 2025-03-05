3094966aefd9e5ac0d998e8fefb3a0c13da653a1
Hayk Ziroyan <haik.ziroan@gmail.com>
2025-03-06 01:49:37 +0400

    make migrations

---
+++ database/migrations/2025_03_05_174044_create_submenu_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSubmenuTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('submenu', function (Blueprint $table) {
+            $table->id();
+            $table->text('name');
+            $table->text('url');
+            $table->unsignedBigInteger('menu_id');
+            $table->foreign('menu_id')->references('id')->on('menu');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('submenu');
+    }
+}
